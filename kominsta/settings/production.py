fb4892bf793bd402885145e156a3a13a0ce3a5ec
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-01-31 13:53:23 +0700

    first commit

--- kominsta/settings/production.py
+++ kominsta/settings/production.py
@@ -2,6 +2,12 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['diskominstamagelangkota.eu.pythonanywhere.com']
+
+ROOT_URLCONF = "kominsta.urls"
+
+SECRET_KEY = "2b_938l%h4*h*r&=xqa_r4zus9spj8m-vq$j=qwk^2kq5irzb9"
+
 try:
     from .local import *
 except ImportError:
